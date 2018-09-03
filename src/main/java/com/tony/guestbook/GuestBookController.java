882c25e2831d08e1c3e3ccbadcfe62a213163570
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-09-03 12:55:02 +0100

    Initial Hello World

---
+++ src/main/java/com/tony/guestbook/GuestBookController.java
@@ -0,0 +1,13 @@
+package com.tony.guestbook;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuestBookController {
+
+    @GetMapping
+    public String testMapping () {
+        return "Hello, World";
+    }
+}
