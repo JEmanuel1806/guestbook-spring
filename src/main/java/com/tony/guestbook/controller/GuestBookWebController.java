7323e4a388370641b9f5a51f32fb3fef990f7366
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-09-04 13:45:25 +0100

    Display entries in HTML table

--- src/main/java/com/tony/guestbook/controller/GuestBookWebController.java
+++ src/main/java/com/tony/guestbook/controller/GuestBookWebController.java
@@ -1,15 +1,26 @@
 package com.tony.guestbook.controller;
 
+import com.tony.guestbook.service.GuestBookService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GuestBookWebController {
 
+    @Autowired
+    private GuestBookService guestBookService;
+
+    private static final String ENTRIES_TEMPLATE_ID = "entries";
     private static final String GUESTBOOK_TEMPLATE = "guestbook";
 
     @GetMapping ("/")
-    public String displayGuestBook () {
+    public String displayGuestBook (Model model) {
+
+        model.addAttribute (ENTRIES_TEMPLATE_ID, this.guestBookService.findAllEntries ());
+
         return GUESTBOOK_TEMPLATE;
     }
 }
