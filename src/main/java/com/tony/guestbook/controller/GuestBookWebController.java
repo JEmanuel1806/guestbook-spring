be4474be9a6583007baf80071dbb22b1931c1abe
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-09-04 15:02:32 +0100

    Added validation to creation

--- src/main/java/com/tony/guestbook/controller/GuestBookWebController.java
+++ src/main/java/com/tony/guestbook/controller/GuestBookWebController.java
@@ -6,10 +6,13 @@ import com.tony.guestbook.service.GuestBookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class GuestBookWebController {
@@ -40,11 +43,18 @@ public class GuestBookWebController {
     }
 
     @PostMapping("/")
-    public String addComment (@ModelAttribute (NEW_ENTRY_TEMPLATE_ID) GuestBookEntry newEntry) {
+    public String addComment (Model model,
+                              @Valid @ModelAttribute (NEW_ENTRY_TEMPLATE_ID) GuestBookEntry newEntry,
+                              BindingResult bindingResult) {
 
-        this.guestBookService.save (newEntry);
-
-        return HOMEPAGE_REDIRECT;
+        if (!bindingResult.hasErrors ()) {
+            this.guestBookService.save (newEntry);
+            return HOMEPAGE_REDIRECT;
+        }
+        else {
+            model.addAttribute (ENTRIES_TEMPLATE_ID, this.guestBookService.findAllEntries ());
+            return GUESTBOOK_TEMPLATE;
+        }
     }
 
 }
