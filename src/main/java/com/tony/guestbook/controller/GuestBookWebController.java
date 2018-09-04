3598c3a2f1b7e9ab896db1d4816fa0a4344a46a5
Tony Jenkins <tony.jenkins@elder-studios.co.uk>
2018-09-04 11:35:37 +0100

    Move guestbook template name to constant

--- src/main/java/com/tony/guestbook/controller/GuestBookWebController.java
+++ src/main/java/com/tony/guestbook/controller/GuestBookWebController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GuestBookWebController {
 
+    private static final String GUESTBOOK_TEMPLATE = "guestbook";
+
     @GetMapping ("/")
     public String displayGuestBook () {
-        return "guestbook";
+        return GUESTBOOK_TEMPLATE;
     }
 }
